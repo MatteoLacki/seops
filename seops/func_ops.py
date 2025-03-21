af7f1a2621f984b4402f6fc300be3a4ac00cd17d
Mateusz Lacki <matteo.lacki@gmail.com>
2025-03-21 10:55:06 +0100

    added doc strings to compose_from_{left,right}

--- seops/func_ops.py
+++ seops/func_ops.py
@@ -2,8 +2,16 @@ import functools
 
 
 def compose_from_left(*funcs):
+    """Compose functions from left to right.
+
+    So that compose(f,g,h)(x) == h(g(f(x)))
+    """
     return lambda x: functools.reduce(lambda v, f: f(v), funcs, x)
 
 
 def compose_from_right(*funcs):
+    """Compose functions from right to left.
+
+    So that compose(f,g,h)(x) == f(g(h(x)))
+    """
     return lambda x: functools.reduce(lambda v, f: f(v), reversed(funcs), x)
